9385b4302dbf9cafb7e0da2c79c7344873c2a624
Glauton Osório <glautonleao@gmail.com>
2024-02-07 17:16:39 -0300

    feat: Form para mudar cor do body

--- Modulo 1/Week 1/js.js
+++ Modulo 1/Week 1/js.js
@@ -1,5 +1,7 @@
 const clickArticle = document.getElementById("article-info");
-const bodyColor = document.getElementById("bg-color");
+const inputColor = document.getElementById("input-color");
+const btnChangeColor = document.getElementById("btn-new-color");
+const btnResetColor = document.getElementById("btn-reset");
 const body = document.body;
 
 console.log(body.style);
@@ -7,10 +9,17 @@ clickArticle.addEventListener("click", () => {
   alert("WELCOME TO JURASSIC PARK BEATCH");
 });
 
-body.addEventListener("click", () => {
+/*body.addEventListener("click", () => {
   if (body.style.backgroundColor === "rgb(255, 192, 203)") {
     body.style.backgroundColor = "#ffdead";
   } else {
     body.style.backgroundColor = "rgb(255, 192, 203)";
   }
+});*/
+
+btnResetColor.addEventListener("click", () => {
+  body.style.backgroundColor = "#ffc0cb";
+});
+btnChangeColor.addEventListener("click", () => {
+  body.style.backgroundColor = `${inputColor.value}`;
 });
